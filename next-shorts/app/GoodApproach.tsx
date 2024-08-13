9a00caa25a3360b591df71654b4c520d8576c5bc
Tobias Meyhöfer <tobias.meyhoefer02@gmail.com>
2024-08-13 15:31:52 +0200

    idk

---
+++ next-shorts/app/GoodApproach.tsx
@@ -0,0 +1,19 @@
+const GoodApproach = () => {
+  return (
+    <section className="p-20">
+      <h1>Der Erlkönig</h1>
+      <p className="text-sm mb-4 text-zinc-500 font-light">
+        A poem from Johann Wolfgang von Goethe, 1782
+      </p>
+      <p>
+        Wer reitet so spät durch Nacht und Wind? Es ist der Vater mit seinem
+        Kind; Er hat den Knaben wohl in dem Arm, Er fasst ihn sicher, er hält
+        ihn warm. Mein Sohn, was birgst du so bang dein Gesicht? – Siehst,
+        Vater, du den Erlkönig nicht? Den Erlenkönig mit Kron’ und Schweif? –
+        Mein Sohn, es ist ein Nebelstreif.
+      </p>
+    </section>
+  )
+}
+
+export default GoodApproach
---
+++ next-shorts/app/page.tsx
@@ -0,0 +1,11 @@
+import BadApproach from "./BadApproach";
+import BetterApproach from "./BetterApproach";
+import GoodApproach from "./GoodApproach";
+
+export default function Home() {
+  return (
+    // <BadApproach/>
+    // <GoodApproach/>
+    <BetterApproach/>
+  )
+}
